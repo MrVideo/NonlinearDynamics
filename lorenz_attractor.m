7cd248fecb5292fa7ba8a49f007e5f4b696bf409
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2024-10-15 00:18:29 +0200

    Add Lorenz attractor simulation

---
+++ lorenz_attractor.m
@@ -0,0 +1,30 @@
+
+% System definition
+function dydt = lorenz(~, y)
+    % Parameters
+    sigma = 10;
+    beta = 8 / 3;
+    rho = 28;
+
+    % Equation system
+    dy1 = sigma * (y(2) - y(1));
+    dy2 = rho * y(1) - y(1) * y(3) - y(2);
+    dy3 = y(1) * y(2) - beta * y(3);
+
+    % Return value
+    dydt = [dy1; dy2; dy3];
+end
+
+% Initial conditions
+y0 = [1; 1; 1];
+
+% Simulation time span
+t = [0 1000];
+
+% Solve
+[t, y] = ode45(@lorenz, t, y0);
+
+% Phase plane
+figure;
+plot3(y(:, 1), y(:, 2), y(:, 3));
+grid on;
