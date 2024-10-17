1179275bf0531b1c6d44425de440f008a1faf132
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2024-10-17 13:38:37 +0200

    Add variable for loop iterations

--- henon_attractor.m
+++ henon_attractor.m
@@ -2,6 +2,9 @@
 % x(t+1) = y(t) + 1 - 1.4x(t)^2
 % y(t+1) = 0.3x(t)
 
+% Number of loop iterations
+iterations = 1000;
+
 % Initial conditions
 x0 = 1.0;
 y0 = 1.0;
@@ -14,7 +17,7 @@ hold on
 x_prev = x0;
 y_prev = y0;
 
-for i = 0:10000
+for i = 0:iterations
     x_next = y_prev + 1 - 1.4 * x_prev^2;
     y_next = 0.3 * x_prev;
 
